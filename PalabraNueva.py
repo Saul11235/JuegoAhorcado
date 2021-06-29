8fec3aabc82bf99fca02d910e93e1b87f7fada94
Edwin Saul <edwinsaulpm@gmail.com>
2021-06-29 00:49:31 -0500

    funcion de palabra aleatoria creada

--- PalabraNueva.py
+++ PalabraNueva.py
@@ -1,6 +1,19 @@
+from random import choice 
+
+ArchivoExterno=open("PALABRAS","r")
+ListaPalabras=[]
+
+for palabra in ArchivoExterno:
+    PalabraCorregida=palabra.upper().removesuffix("\n")
+    if PalabraCorregida!="":ListaPalabras.append(PalabraCorregida)
+
+print(ListaPalabras)
+#corregir
+
 
 def PalabraNueva():
-    return("PALABRA")
+    try: return(choice(ListaPalabras))
+    except:return("PALABRAS")
 
 if __name__=="__main__":
     print(PalabraNueva())
