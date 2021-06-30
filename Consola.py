4197e01ba08d847fb152a8234b935f8f9f02382c
Edwin Saul <edwinsaulpm@gmail.com>
2021-06-29 23:35:24 -0500

    finalizado juego en modo consola

---
+++ Consola.py
@@ -0,0 +1,21 @@
+from ClaseJuego import ClaseJuego
+
+class JuegoModoConsola:
+
+    def __init__(self):
+        self.juego=ClaseJuego()
+        self.EstamosJugando=False
+        self.EntradaConsola=""
+        
+        while true: pass
+            
+
+    def jugar():
+        print("jugando")
+        
+    def __pantallazoEnEspera(self):
+        pass
+
+if __name__=="__main__":
+    j=JuegoModoConsola()
+    
