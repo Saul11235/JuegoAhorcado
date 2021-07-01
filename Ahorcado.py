6588ea1add2a503a97f50d3794045570115f20cb
Edwin Saul <edwinsaulpm@gmail.com>
2021-07-01 10:05:17 -0500

    commit de prueba

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,35 +1,26 @@
-from ClaseJuego import ClaseJuego 
 from tkinter import *
+from ClaseJuego import ClaseJuego 
 
-class JuegoGUI:
-
-    def __init__(self):
-        self.juego=ClaseJuego()
-        self.primerjuego=True
-        self.EstamosJugando=False
-        self.ventana=Tk()
-        
-    def maquetarVentana(self):
-        self.botonReinicio=Button(self.ventana,text="Nuevo Juego",command=lambda:self.NuevoJuego()).pack()
-        self.EtiquetaTexto=Label(self.ventana,text="P A L A B R A").pack()
-        self.EtiquetaTexto=Label(self.ventana,text="tus jugadas son").pack()
-        self.BotonEnviar=Button(self.ventana,text="texto",command=lambda:self.enviarTexto()).pack()
-        pass
-
-    def LanzarPantalla(self): self.maquetarVentana(); self.ventana.mainloop()    
-
-    def ActualizarPantalla(self):
-        pass
-
-    def enviarTexto(self):
-        print("texto enviado")
-        pass
-
-    def NuevoJuego(self):
-        print("nuevo juego creado")
-        self.juego.nuevojuego()
+juego=ClaseJuego()
 
 
-if __name__=="__main__":
-    j=JuegoGUI().LanzarPantalla()
+ventana=Tk()
 
+ventana.title("Juego del ahorcado")
+ventana.resizable(width=False,height=False)
+ventana.geometry("600x300")
+
+BotonNuevoJuego=Button(ventana,text="Nuevo Juego",command=lambda:NuevoJuego()).pack()
+Letrero=Label(ventana,text="hola").pack()
+
+
+def EnviarTexto():
+    print("algo copado")
+
+def NuevoJuego():
+    print("nuevo juego")
+    print(Letrero)
+    juego.nuevojuego()
+
+
+ventana.mainloop()
